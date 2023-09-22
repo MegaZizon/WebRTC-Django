b8713b342da5221fd72397b13bb7056b88704ade
MegaZizon <wnsdh8846@naver.com>
2023-09-23 05:30:24 +0900

    Project Done.

---
+++ test.py
@@ -0,0 +1,9 @@
+def get_json():
+    json_result = {}
+    json_result['test1'] = "python1"
+    json_result['test2'] = "python2"
+    json_result['test3'] = "python3"
+    
+    return json_result
+    
+print(get_json())
